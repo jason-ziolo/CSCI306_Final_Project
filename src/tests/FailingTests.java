7cc5c4ad76c95f844c65b4af70896c29765ecf1a
syoshiha <syoshiha@mymail.mines.edu>
2015-11-24 14:57:32 -0700

    TestCore2 test

--- src/tests/FailingTests.java
+++ src/tests/FailingTests.java
@@ -134,9 +134,17 @@ public class FailingTests {
 	@Test
 	public void TestCore2(){
 		Fraction core2Fraction1 = new Fraction(1,2);
-		Fraction core2Fraction2 = new Fraction(3,4);
+		Fraction core2Fraction2 = new Fraction(2,4);
 		assertTrue(core2Fraction1.getNumerator() / core2Fraction1.getDenominator() 
 				== core2Fraction2.getNumerator() / core2Fraction2.getDenominator());
+		Fraction core2Fraction3 = new Fraction(1,5);
+		Fraction core2Fraction4 = new Fraction(3,4);
+		assertTrue(core2Fraction3.getNumerator() / core2Fraction3.getDenominator() 
+				< core2Fraction4.getNumerator() / core2Fraction4.getDenominator());
+		Fraction core2Fraction5 = new Fraction(2,3);
+		Fraction core2Fraction6 = new Fraction(1,4);
+		assertTrue(core2Fraction5.getNumerator() / core2Fraction5.getDenominator() 
+				== core2Fraction6.getNumerator() / core2Fraction6.getDenominator());
 		
 	}
 	
