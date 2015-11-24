4b59c690d210a0a614b537f57fe9b1c0a47adafa
jason-ziolo <jziolo@mymail.mines.edu>
2015-11-24 12:04:46 -0700

    added skeleton functions to fraction class

--- src/game/Fraction.java
+++ src/game/Fraction.java
@@ -1,5 +1,19 @@
 package game;
 
 public class Fraction {
-
+	private int numerator;
+	private int denominator;
+	
+	public Fraction(int num, int den) {
+		numerator = num;
+		denominator = den;
+	}
+	
+	private double getValue() {
+		return 0.0; //TODO
+	}
+	
+	public boolean equals(Fraction other) {
+		return false; //TODO
+	}
 }
